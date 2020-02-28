312cdc296cbc82672c76c9f5d8b64f92c2f75694
SihYi Lin <sihyiyl2@illinois.edu>
2020-02-28 01:30:50 -0600

    ✨ Feat: 1. Focus to content 2. LocalStorage username
    1. Focus to content input after page loaded to enhance user experience
     2. Store username to
    localStorage and automatically fill in the username field after page loaded

--- src/components/CommentInput.js
+++ src/components/CommentInput.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 import '../styles/CommentInput.scss'
 
 class CommentInput extends Component {
+  static propTypes = {
+    onSubmit: PropTypes.func
+  }
+
   constructor () {
     super()
     this.state = {
@@ -10,14 +15,37 @@ class CommentInput extends Component {
     }
   }
 
-  handleUserName(event) {
-      this.setState({username: event.target.value})
+  // Do maipulations that don't rely on DOM
+  componentWillMount() {
+    this._loadUsername()
+  }
+  // Do maipulations that do rely on DOM
+  componentDidMount() {
+    this.textarea.focus()
   }
 
+  // every private method(only be called inside this class / helper functions) should named begin with '_'
+  _saveUsername(username) {
+    localStorage.setItem('username', username)
+  }
+  _loadUsername() {
+    const username = localStorage.getItem('username')
+    if (username) {
+      this.setState({username: username})
+    }
+  }
+
+  // every method that use for 'on-' event should named begin with 'handle'
+  handleUsernameChange(event) {
+      this.setState({username: event.target.value})
+  }
+  handleUsernameBlur(event) {
+    this._saveUsername(event.target.value)
+  }
   handleContent(event) {
     this.setState({content: event.target.value})
 	}
-	
+  // Receive the onSubmit method from parent component
 	handleSubmit() {
 		if (this.props.onSubmit) {
 			const {username, content} = this.state
@@ -32,13 +60,19 @@ class CommentInput extends Component {
           <div className='comment-field'>
               <span className='comment-field-name'>UserName:</span>
               <span className='comment-field-input'>
-                  <input value={this.state.username} onChange={this.handleUserName.bind(this)}/>
+                  <input 
+                    value={this.state.username} 
+                    onChange={this.handleUsernameChange.bind(this)} 
+                    onBlur={this.handleUsernameBlur.bind(this)}/>
               </span>
           </div>
           <div className='comment-field'>
               <span className='comment-field-name'>Comment:</span>
               <div className='comment-field-input'>
-                <textarea value={this.state.content} onChange={this.handleContent.bind(this)}/>
+                <textarea 
+                  ref={(textarea) => this.textarea = textarea}
+                  value={this.state.content} 
+                  onChange={this.handleContent.bind(this)} />
               </div>
           </div>
           <div className='comment-field-button'>
